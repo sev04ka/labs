c9653c2c18138b9fccf5a859abb44f9dbe87fe08
sev04ka <seva.belyaev04@gmail.com>
2023-03-19 18:50:43 +0300

    добавлены 1 и 3 работы

---
+++ LabN1/LabN1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите 5 чисел");
+double num1 = Convert.ToDouble(Console.ReadLine());
+double num2 = Convert.ToDouble(Console.ReadLine());
+double num3 = Convert.ToDouble(Console.ReadLine());
+double num4 = Convert.ToDouble(Console.ReadLine());
+double num5 = Convert.ToDouble(Console.ReadLine());
+double result = (num1 + num2 + num3 + num4 + num5)/5;
+Console.WriteLine(result);
---
+++ LabN3/LabN3/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Diagnostics;
+using System.Linq.Expressions;
+
+void numoperations()
+{
+    Console.WriteLine("Введите число больше 0");
+    string numinput = Console.ReadLine();
+    bool validation = int.TryParse(numinput, out int number);
+    if (validation = true && number > 0)
+    {
+        operselection();
+    }
+    else
+    {
+        Console.WriteLine("Некорректный ввод");
+        numoperations();
+    }
+    void operselection()
+    {
+        Console.WriteLine("Нажмите 1 для получения факториала введённого числа");
+        Console.WriteLine("Нажмите 2 для получения числа из ряда Фибоначчи с введённым порядковым номером");
+        var keycheck = Console.ReadKey().Key;      
+        switch (keycheck)
+        {
+            case ConsoleKey.D1:
+                Factorio(number);
+                break;
+            case ConsoleKey.D2:
+                Console.WriteLine();
+                Console.WriteLine(Fibonachi(number));
+                break;
+        }
+        void Factorio(int number)
+        {
+            int resFact = 1;
+            for (int i = 1; i <= number; i++)
+            {
+                resFact *= i;
+            }
+            Console.WriteLine();
+            Console.WriteLine(resFact);
+        }
+        int Fibonachi(int number)
+        {
+            if (number == 1 || number == 2 ) return number - 1;
+            return Fibonachi(number - 1) + Fibonachi(number - 2);
+        }
+    }
+}
+Console.WriteLine("Нажмите enter для начала или esc для закрытия приложения");
+var exitstart = Console.ReadKey().Key;
+switch (exitstart)
+{
+    case ConsoleKey.Enter:
+        numoperations();
+        break;
+    case ConsoleKey.Escape:
+        Environment.Exit(0);
+        break;
+}
